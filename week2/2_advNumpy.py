12a17e6b72fdfc84776d310aeed72a51445bee08
selo <ahmadseloabadi@gmail.com>
2025-01-05 16:24:39 +0700

    rename file

---
+++ week2/2_advNumpy.py
@@ -0,0 +1,14 @@
+# broadcasting in numpy
+
+import numpy as np
+
+print('\n------broadcasting in numpy-----\n')
+
+# rules for broadcasting:
+# -> dimensions are aligned from the right
+# -> a dimension is compatible if:
+# --> it matches the other array's dimensions
+# --> one of the dimensions is 1
+
+
+

f31e4e5aacf1b65606947b2643a8e94255ec1193
selo <ahmadseloabadi@gmail.com>
2025-01-05 22:44:02 +0700

    continue intro pandas

--- week2/3_introPandasForDataStructur.py
+++ week2/3_introPandasForDataStructur.py
@@ -17,6 +17,22 @@ data={"Name":["sungep","luna"],"age":[20,25]}
 df = pd.DataFrame(data)
 print('\ninitial pandas dataframe :\n',df)
 
+# loading data from csv,exel,and other source files
+print('\n -----Pandas loading data------\n')
+
+file_csv=pd.read_csv('week2/data/contoh_file.csv')
+
+print('\n load data csv with pandas : \n',file_csv)
+
+file_excel=pd.read_excel('week2/data/contoh_file.xlsx')
+
+print('\n load data xlsx with pandas : \n',file_excel)
+
+print('\n -----Pandas save data------\n')
+
+
+
+
 
 
 

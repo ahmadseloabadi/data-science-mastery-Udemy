5adc3fb64bbbf9aa66cfd272c99a09849581d799
selo <ahmadseloabadi@gmail.com>
2025-01-07 02:49:26 +0700

    continue intro pandas

--- week2/3_introPandasForDataStructur.py
+++ week2/3_introPandasForDataStructur.py
@@ -30,6 +30,25 @@ print('\n load data xlsx with pandas : \n',file_excel)
 
 print('\n -----Pandas save data------\n')
 
+# Membuat data
+data2 = {
+    'Nama': ['Andi', 'Budi', 'Citra', 'Dewi'],
+    'Umur': [25, 30, 22, 27],
+    'Pekerjaan': ['Programmer', 'Desainer', 'Dokter', 'Guru'],
+    'Gaji': [7000000, 8000000, 10000000, 6000000]
+}
+
+# Membuat DataFrame
+df2 = pd.DataFrame(data2)
+
+path_csv = 'week2/data/save_data_csv.csv'
+path_excel = 'week2/data/save_data_excel.xlsx'
+
+df2.to_csv(path_csv)
+print('\n success save data to csv on ' + path_csv)
+df2.to_excel(path_excel)
+print('\n success save data to excel on ' + path_excel)
+
 
 
 

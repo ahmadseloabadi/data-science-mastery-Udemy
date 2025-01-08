293f73fe533484e46a95a0a775a38adc7d6bd249
selo <ahmadseloabadi@gmail.com>
2025-01-09 01:53:02 +0700

    exploratory data analysis

---
+++ week2/7_exploratoryDataAnalysis.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv')
+
+print('\n initial dataset:\n',df.head())
+
+print('\n dataset info with info():\n')
+print(df.info())
+
+print('\n dataset info with describe():\n')
+print(df.describe())
+
+# handle missing values
+df['Age'] = df['Age'].fillna(df['Age'].median())
+
+print('\n fill Age column with median values:\n',df['Age'])
+
+df['Embarked'] = df['Embarked'].fillna(df['Embarked'].mode()[0]) # mode() digunakan untuk menghitung modus, yaitu nilai yang paling sering muncul dalam kolom Embarked dan [0] adalah nilai pertama dari hasil mode(), yang merupakan nilai modus utama
+
+print('\n fill Embarked column with .mode()[0] :\n',df['Embarked'])
+
+# remove duplicates
+
+df = df.drop_duplicates()
+
+print('\n remove duplicates in dataset :\n',df)
+
+print('\n info dataset after removing duplicates:\n')
+print(df.info())
+
+# filter data :  passanger in first class
+
+first_class= df[df['Pclass'] == 1]
+
+print('\n filter passanger in first class:\n', first_class)
+

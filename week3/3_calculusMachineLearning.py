5472ae41da6ffa06631a6845ae853fdbd0d1c7ce
selo <ahmadseloabadi@gmail.com>
2025-01-12 02:27:23 +0700

    continue calculuss machine learning

---
+++ week3/3_calculusMachineLearning.py
@@ -0,0 +1,30 @@
+# introduction to derivatives(turunan) and their role in optimization
+import sympy as sp
+
+x = sp.Symbol('x')
+f = x**2
+derivative = sp.diff(f,x)
+
+print('\n derivative:',derivative)
+
+#  parcial derivative
+x ,y= sp.symbols('x y')
+f = x**2 + y**2
+gradient_x = sp.diff(f,x)
+gradient_y = sp.diff(f,y)
+
+print('\n parcial derivative:',gradient_x,gradient_y)
+
+# gradirent descent optimation algorithm
+
+# exercise
+
+# define simple derivatives function
+
+x = sp.Symbol('x')
+f = x**3 - 5*x + 7
+
+# compute derivatives
+derivative_exer = sp.diff(f,x)
+
+print('\n derivatives:',derivative_exer)
